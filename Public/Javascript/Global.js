7073c81f2c9c01653e2d2323f43bd5e734640157
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-11-23 09:35:51 +0100

    Added sidebar which displays more information about wine

--- Public/Javascript/Global.js
+++ Public/Javascript/Global.js
@@ -27,13 +27,18 @@ function populateTable(){
         // For each Loop here
         $.each(data, function(){
             WineBoxes += '<div class="wine-display-container container position-relative d-inline-flex m-3 p-0 shadow bg-light rounded"';
-            WineBoxes += 'style="height:400px!important; overflow:hidden; flex:1 1 400px; background:url(\'/Images/'+ this.artNum +'.jpg\') center no-repeat; background-size:auto 80%;">'
-            WineBoxes +=    '<div class=" container d-flex flex-column justify-content-between">'
-            WineBoxes +=        '<div class="container p-2 pb-4">' + this.name + '</div>'
-            WineBoxes +=        '<div class="container p-2 pt-4 text-right">' + this.price + ' Kr</div>'
-            WineBoxes +=    '</div>'
-            WineBoxes +=    '<div class="wine-display-more-info container d-block position-absolute align-self-end p-2" style="margin-bottom:-50px">' + this.artNum + '</div>'
-            WineBoxes += '</div>'
+            WineBoxes += 'style="height:400px!important; overflow:hidden; flex:1 1 400px; background:url(\'/Images/'+ this.artNum +'.jpg\') center no-repeat; background-size:auto 80%;">';
+            WineBoxes +=    '<div class=" container d-flex flex-column justify-content-between">';
+            WineBoxes +=        '<div class="container p-2 pb-4">' + this.name + '</div>';
+            WineBoxes +=        '<div class="container p-2 pt-4 text-right">' + this.price + ' Kr</div>';
+            WineBoxes +=    '</div>';
+            WineBoxes +=    '<div class="wine-display-more-info container d-block position-absolute align-self-end p-2" style="margin-bottom:-50px">' + this.artNum + '</div>';
+            WineBoxes +=    '<div class="wine-display-more-info-click container d-block position-absolute p-2 h-100"style=" width:30%;right:0;">';
+            WineBoxes +=        '<b>Land:</b><br>' + this.origin;
+            WineBoxes +=        '<br><b>Region:</b><br>' + this.region;
+            WineBoxes +=        '<br><b>Druvor:</b><br>' + this.grape.join(", ");
+            WineBoxes +=    '</div>';
+            WineBoxes += '</div>';
         });
         // Insert into existing html table
         $('#wine-container').append(WineBoxes);
